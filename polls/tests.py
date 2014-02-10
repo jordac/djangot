dbd1f03898485b2a8c4be36709a74b18970fe0f3
jordac <jordac90@gmail.com>
2014-02-09 23:30:29 -0800

    added more tests... need to fill some. also added new polls!

--- polls/tests.py
+++ polls/tests.py
@@ -1,8 +1,18 @@
 import datetime
 from django.utils import timezone
 from django.test import TestCase
-
+from django.core.urlresolvers import reverse
+from django.test.client import Client
 from polls.models import Poll
+
+def create_poll(question, days):
+	"""
+	Creates a poll with the given 'question' published the given number of
+	'days' offset to now (negative for polls published in the past,
+	positive for polls that have yet to be published)
+	"""
+	return Poll.objects.create(question=question,
+		pub_date=timezone.now() _ datetime.timedelta(days=days))
 
 class PollMethodTests(TestCase):
 
@@ -27,3 +37,23 @@ class PollMethodTests(TestCase):
 			"""
 			recent_poll = Poll(pub_date=timezone.now() - datetime.timedelta(hours=1))
 			self.assertEqual(recent_poll.was_published_recently(), True)
+
+class PollViewTests(TestCase):
+	def test_index_view_with_no_polls(self):
+		"""
+		if no polls exist, an appropriate message should be displayed
+		"""
+		response = self.client.get(reverse)
+		self.assertEqual(response.status_code=200)
+		self.assertContains(response, "No polls are available.")
+		self.assertQuerysetEqual(response.context['latest_poll_list']), [])
+	def test_index_view_with_a_past_poll(self):
+		pass
+	def test_index_view_with_a_future_poll(self):
+		pass
+	def test_index_view_with_future_poll_and_past_poll(self):
+		pass
+	def test_index_view_with_two_past_polls(self):
+		pass
+
+
